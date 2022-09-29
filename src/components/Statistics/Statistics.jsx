f1d47b9ec5a3065cacdef335eff1df7ba5be7189
Олег Бакалов <O.O.Bakalov@gmail.com>
2022-09-29 17:46:46 +0300

    statistics

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+
+export function Statistics({ good, neutral, bad, total, positivePercentage }) {
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <div>
+        <span>Good: {good}</span>
+        <span>Neutral: {neutral}</span>
+        <span>Bad: {bad}</span>
+        <span>total: {total}</span>
+        <span>positivePercentage: {positivePercentage} %</span>
+      </div>
+    </div>
+  );
+}
